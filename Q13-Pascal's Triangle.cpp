140c0ebc5b942682210a28eb403f15054e0dc804
Sanskar-eng <54894537+Sanskar-eng@users.noreply.github.com>
2022-01-11 21:34:25 +0530

    Create Q13-Pascal's Triangle.cpp

---
+++ Q13-Pascal's Triangle.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int binomialCoeff(int n, int k)
+{
+    int res = 1;
+    if (k > n - k)
+    k = n - k;
+    for (int i = 0; i < k; ++i)
+    {
+        res *= (n - i);
+        res /= (i + 1);
+    }
+    
+    return res;
+}
+int main()
+{
+  int n;
+  cin>>n;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j <= i; j++)
+            cout <<" "<< binomialCoeff(i, j);
+        cout <<"\n";
+    }
+    return 0;
+}
