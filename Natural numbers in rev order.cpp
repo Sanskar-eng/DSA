b828f7fa45918994e0b8c6554951f705d2796a49
Sanskar-eng <54894537+Sanskar-eng@users.noreply.github.com>
2022-01-12 17:04:37 +0530

    Create Natural numbers in rev order.cpp

---
+++ Natural numbers in rev order.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+void printRev(int n){
+    if(n==0)
+    return;
+    cout<<n<<" ";
+    printRev(n-1);
+}
+int main(){
+    int n;
+    cin>>n;
+    printRev(n);
+    return 0;
+}
